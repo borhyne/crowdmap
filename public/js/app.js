e2e644fc96bd4243513495c3792cabd7bb391772
bo <bo.rhyne1@gmail.com>
2016-12-17 21:24:59 -0800

    come on

--- public/js/app.js
+++ public/js/app.js
@@ -91,7 +91,6 @@ angular.module("contactsApp", ['ngRoute','ui-leaflet'])
             });
         }
 
-        $scope.Contacts = Contacts.data;
         /*
         {
             "marker":{
@@ -126,7 +125,7 @@ angular.module("contactsApp", ['ngRoute','ui-leaflet'])
                 lng: -122.44,
                 zoom: 12
             },
-            markers: Contacts,
+            markers: $scope.contacts = contacts.data,
             position: {
                 lat: 37.77,
                 lng: -122.435
