2993e9d63f7256cf5cbd21726ac4a7e785f0ae81
bo <bo.rhyne1@gmail.com>
2016-12-17 18:21:13 -0800

    testing

--- public/js/app.js
+++ public/js/app.js
@@ -91,7 +91,7 @@ angular.module("contactsApp", ['ngRoute','ui-leaflet'])
             });
         }
 
-        var themarker = Contacts;
+        var themarker = contacts;
         /*
         {
             "marker":{
