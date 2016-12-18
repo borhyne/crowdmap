a0008c9516c4f9f0a7d42e4f20564e5706dba556
bo <bo.rhyne1@gmail.com>
2016-12-18 00:26:15 -0800

    come on

--- public/js/app.js
+++ public/js/app.js
@@ -77,7 +77,7 @@ angular.module("contactsApp", ['ngRoute','ui-leaflet'])
     .controller("ListController", function(contacts, $scope) {
         $scope.contacts = contacts.data;
     })    
-    .controller('MarkersSimpleController', ['$scope','$location', 'Contacts', '$http', function ($scope, $location, Contacts, $http, contacts) {
+    .controller('MarkersSimpleController', ['$scope','$location', 'Contacts', '$http', 'contacts' function ($scope, $location, Contacts, $http, contacts) {
         $scope.back = function() {
             $location.path("#/contact");
         }
