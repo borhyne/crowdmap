fe6785c1a70ba26176c910abe0aa46148bd76095
bo <bo.rhyne1@gmail.com>
2016-12-18 22:49:13 -0800

    come on

--- public/js/app.js
+++ public/js/app.js
@@ -110,7 +110,7 @@ angular.module("contactsApp", ['ngRoute','ui-leaflet'])
         $http.get('/contacts').
             success(function(data) {
                 $scope.help = data[0];
-                alert(JSON.stringify(data[0]))
+                alert(JSON.stringify(data))
             });
 
         angular.extend($scope, {
