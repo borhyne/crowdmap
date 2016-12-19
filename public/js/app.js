1a1692a697383a04703e99717574171b79cd182c
bo <bo.rhyne1@gmail.com>
2016-12-18 20:01:28 -0800

    come on

--- public/js/app.js
+++ public/js/app.js
@@ -111,7 +111,7 @@ angular.module("contactsApp", ['ngRoute','ui-leaflet'])
 
         $http.get('/contacts').
             success(function(data, status, headers, config) {
-                var posts = data;
+                var posts = data[0];
                 alert(JSON.stringify(posts, null, 4));
             }).
             error(function(contact, status, headers, config) {
