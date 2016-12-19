7088567c9f1b35429a047cf37aa69374a26eafa6
bo <bo.rhyne1@gmail.com>
2016-12-18 21:38:30 -0800

    come on

--- public/js/app.js
+++ public/js/app.js
@@ -116,8 +116,6 @@ angular.module("contactsApp", ['ngRoute','ui-leaflet'])
             alert("problem");
         });
 
-        alert(JSON.stringify(help));
-
         angular.extend($scope, {
             sanfran: {
                 lat: 37.77,
