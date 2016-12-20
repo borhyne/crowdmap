680563a039d7d993768ad159170cd809e4f1114b
bo <bo.rhyne1@gmail.com>
2016-12-19 22:20:09 -0800

    come on

--- public/js/app.js
+++ public/js/app.js
@@ -118,12 +118,12 @@ angular.module("contactsApp", ['ngRoute','ui-leaflet'])
                 $http.get('/contacts').success(function(data, where) {
                     for (var i = 0; i < data.length; i++){
                         if (data[i]._id == "5855fa53942137001165e01d"){
-                            lat: where.lat,
-                            lng: where.lon
+                            lat: where.lat;
+                            lng: where.lon;
                         }
                         else {
-                            lat: data[i].marker.lat,
-                            lng: data[i].marker.lng
+                            lat: data[i].marker.lat;
+                            lng: data[i].marker.lng;
                         }
                         $scope.help.push({
                             lat: lat,
